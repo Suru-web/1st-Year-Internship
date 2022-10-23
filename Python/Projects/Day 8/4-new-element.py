353c49be47fc9efb06e39ce3f652aa1755e52c45
Suraj G Meharwade <66472830+Suru-web@users.noreply.github.com>
2022-10-23 15:27:47 +0530

    Add files via upload

---
+++ Python/Projects/Day 8/4-new-element.py
@@ -0,0 +1,4 @@
+d1={1:'java',2:'python',3:'web','name':'infidata'}
+print(d1)
+d1[2]='machine learning'
+print(d1)
---
+++ Python/Projects/Day 8/7-clear.py
@@ -0,0 +1,9 @@
+country_code = {
+ 'India' : '0091',
+ 'Australia' : '0025',
+ 'Nepal' : '00977'
+ }
+print(country_code)
+country_code.clear()
+print("Dictionary after clearning :",country_code)
+
