d274947025316498fbce4d47fe50fe8e50e72c03
Suraj G Meharwade <66472830+Suru-web@users.noreply.github.com>
2022-10-23 15:11:17 +0530

    Add files via upload

---
+++ Python/Day 8/DSA_Dictionary/2_dict_access.py
@@ -0,0 +1,7 @@
+d1={'name':'suraj','branch':'cse','sem':3}
+print(d1)
+
+print(d1.get('name'))
+print(d1['branch'])
+print(d1.get('sem'))
+print(d1.get(10))
---
+++ Python/Day 8/DSA_Dictionary/8_dict_iterator.py
@@ -0,0 +1,8 @@
+num={1:10,2:20,3:30,4:40}
+print(num)
+
+for i in num:
+    print(num[i])
+
+
+
